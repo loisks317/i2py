90aec3c095ff29f9ed90120d2a929e33bb8fae66
cstawarz <cstawarz@83c653a0-9053-0410-b386-6fe0bd4d5a62>
2005-05-27 18:42:53 +0000

    Cleaned up maplib.py

--- branches/lalr-branch/i2py/maplib.py
+++ branches/lalr-branch/i2py/maplib.py
@@ -18,29 +18,54 @@
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
+
 """
-This file defines mappings for some builtin IDL functions and procedures.  This
-will need to be expanded quite a bit before the package is ready for production
-use.
+Defines mappings for some builtin IDL variables, procedures, and functions
 """
 
-from map import map_proc, map_func, map_var
 
-# These are essential to the function mapping mechanism and can't be changed!
-map_func('N_PARAMS', callfunc=(lambda i,o: 'n_params'))
-map_func('KEYWORD_SET', pars=[1],
-         callfunc=(lambda i,o: '(%s is not None)' % i[0]))
+from map import map_var, map_pro, map_func
 
-map_proc('ON_ERROR', inpars=[1],
-         callfunc=(lambda i,o: '# ON_ERROR, %s' % i[0]))
-map_proc('PRINT', inpars=range(1,101), noptional=99, inkeys=['FORMAT'],
-         callfunc=(lambda i,o: 'print ' + ', '.join(i)))
+
+################################################################################
+#
+# Variable maps
+#
+################################################################################
+
+
+# pi is in Numeric/numarray
+map_var('!DPI', 'pi')
+map_var('!RADEG', '_radeg', '_radeg = 180.0 / pi')
+
+
+################################################################################
+#
+# Procedure maps
+#
+################################################################################
+
+
+map_pro('ON_ERROR', inpars=[1],
+        callfunc=(lambda i,o: '# ON_ERROR, %s' % i[0]))
+map_pro('PRINT', inpars=range(1,101), noptional=99, inkeys=['FORMAT'],
+        callfunc=(lambda i,o: 'print ' + ', '.join(i)))
+
+
+################################################################################
+#
+# Function maps
+#
+################################################################################
+
 
 def arrgen(typename):
+   "Returns an array-generation callfunc for type typename"
    return (lambda i,o: 'zeros([%s], %s)' %
            (', '.join([ i[n] for n in xrange(len(i)-1, -1, -1) ]), typename))
 
 def typeconv(typename):
+   "Returns a type-conversion callfunc for type typename"
    return (lambda i,o: 'array(%s, copy=0).astype(%s)' % (i[0], typename))
 
 map_func('DOUBLE', pars=[1], callfunc=typeconv('Float64'))
@@ -57,8 +82,4 @@ map_func('REPLICATE', pars=range(1,10), noptional=7,
 	           ', '.join([ i[n] for n in xrange(len(i)-1, 0, -1) ]))))
 map_func('WHERE', pars=[1,2], noptional=1,
          callfunc=(lambda i,o: 'where(ravel(%s))[0]' % i[0]))
-
-# pi is in numarray
-map_var('!DPI', 'pi')
-map_var('!RADEG', '_radeg', '_radeg = 180.0 / pi')
 
