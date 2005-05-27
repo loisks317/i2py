ba65629bb9884b7a95e3bae398ae7cf7c672a4bc
cstawarz <cstawarz@83c653a0-9053-0410-b386-6fe0bd4d5a62>
2005-05-27 17:10:45 +0000

    Cleaned up util.py

--- branches/lalr-branch/i2py/util.py
+++ branches/lalr-branch/i2py/util.py
@@ -18,24 +18,51 @@
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-idltab = '   '
-pytab  = '   '
+
+"""
+General utility functions
+"""
+
+
+idltab = '   '   # Tab for IDL code
+pytab  = '   '   # Tab for Python code
+
 
 def indent(obj, ntabs=1, tab=None):
+   """
+   Converts obj to a string, pads the beginning of each line with ntabs copies
+   of tab, and returns the result.  If tab is not given, idltab is used.
+   """
    if not tab:  tab = idltab
    pad = ntabs * tab
    return pad + str(obj).replace('\n', '\n' + pad).rstrip(tab)
 
+
 def pycode(obj):
+   """
+   If obj has a pycode() method, returns the result of calling it.  Otherwise,
+   returns obj converted to a string.
+   """
    if hasattr(obj, 'pycode'):
       return obj.pycode()
    return str(obj)
 
+
 def pyindent(obj, ntabs=1, tab=None):
+   """
+   Converts obj to a string of Python code with pycode(), pads the beginning
+   of each line with ntabs copies of tab, and returns the result.  If tab is
+   not given, pytab is used.
+   """
    if not tab:  tab = pytab
    pad = ntabs * tab
    return pad + pycode(obj).replace('\n', '\n' + pad).rstrip(tab)
 
+
 def pycomment(obj):
+   """
+   Calls pyindent() on obj with tab set to '# ' and returns the result.
+   """
    return pyindent(obj, tab='# ')
+
 
